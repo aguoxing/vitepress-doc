a5838b2ab8deec0cd6efc087b6c045a905ffe385
aguoxing <2933575030@qq.com>
2023-03-19 21:27:33 +0800

    chore: update deps

--- docs/.vitepress/config/markdown.ts
+++ docs/.vitepress/config/markdown.ts
@@ -2,10 +2,6 @@ import type { MarkdownOptions } from 'vitepress'
 
 export const markdown: MarkdownOptions = {
   // Shiki主题, 所有主题参见: https://github.com/shikijs/shiki/blob/main/docs/themes.md
-  theme: {
-    light: 'material-palenight',
-    dark: 'one-dark-pro'
-  },
   lineNumbers: true, // 启用行号
 
   // 在所有文档的<h1>标签后添加<ArticleMetadata/>组件
